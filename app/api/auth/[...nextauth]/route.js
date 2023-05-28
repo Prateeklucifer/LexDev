79d0926862dce1b2008f4329bae1d162f2e1e7a9
Prateek <letbeastcome@gmail.com>
2023-05-28 16:47:25 +0530

    fixed problem in next-auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,6 +10,7 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
       let loggingInUser = profile;
